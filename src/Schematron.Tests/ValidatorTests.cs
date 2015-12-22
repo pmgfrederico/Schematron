b45a143baff10e2c411af165f7c4c4364a7da426
Pedro Frederico <pedro.frederico@sky.uk>
2015-12-22 17:30:53 +0000

    Added some tests to prove that .AddSchema overloads are not consistent for import schema scenarios

--- src/Schematron.Tests/ValidatorTests.cs
+++ src/Schematron.Tests/ValidatorTests.cs
@@ -7,17 +7,91 @@ using System.Text;
 using System.Xml.Schema;
 using Xunit;
 
+using ValidationResult = Schematron.Serialization.SchematronValidationResultTempObjectModel.output;
+using System.Diagnostics;
+
 namespace Schematron.Tests
-{    
+{
     public class ValidatorTests
     {
+        public class SchemaLoadingFailedException : Exception { }
+
         const string XsdLocation = "./Content/po-schema.xsd";
         const string XsdWithPartialSchemaLocation = "./Content/po-schema-with-schema-import.xsd";
         const string XmlContentLocation = "./Content/po-instance.xml";
         const string TargetNamespace = "http://example.com/po-schematron";
 
+        // TODO Scenarios to test
+
+        // Should adding schemas phase succeed
+        // 1.0 base schema validation is OK
+            // 1.1 and the schematron validation is OK
+            // 1.2 and the schematron validation is KO
+        // 2.0 base schema validatino is KO
+            // 2.1 and the schematron validation is OK
+            // 2.2 and the schematron validation is KO
+
+        #region .AddSchema(params) - Validation should be independent of the method I choose to add the schemas
+
+        #region Scenario 1. .AddSchema(XmlSchema) - Adding the XmlSchema directly won't account for the schematron annotations
+        
         [Fact]
-        public void NewAddSchemaSignatureShouldNotBreakCode()
+        public void WhenSchemaIsValid_AddSchemaShouldNotThrowXmlValidationException_S1()
+        {
+            var validator = new Schematron.Validator(OutputFormatting.XML);
+
+            using (var schemaXmlReader = XmlReader.Create(XsdWithPartialSchemaLocation))
+            {
+                var schema = XmlSchema.Read(schemaXmlReader, (sender, args) => { throw new SchemaLoadingFailedException(); });
+
+                validator.AddSchema(schema);
+
+                var ex = Xunit.Assert.Throws<Schematron.ValidationException>(() => validator.Validate(XmlContentLocation));
+
+                var result = ex.ToValidationResult();
+
+                Xunit.Assert.NotNull(result.xml);
+                Xunit.Assert.Null(result.schematron);
+            }
+        }
+
+        #endregion
+
+        #region Scenario 2. .AddSchema(Uri) -> .AddSchema(Stream) -> .AddSchema(XmlTextReader)  -> .AddSchema(XmlReader)
+
+        [Fact]
+        public void WhenSchemaIsValid_AddSchemaShouldNotThrowXmlValidationException_S2()
+        {
+            var validator = new Schematron.Validator(OutputFormatting.XML);
+            
+            validator.AddSchema(XsdWithPartialSchemaLocation);
+
+            Xunit.Assert.Throws<Schematron.ValidationException>(() => validator.Validate(XmlContentLocation));
+        }                             
+
+        #endregion
+
+        #region Scenario 3. .AddSchema(TargetNamespace, SchemaUri)
+
+        [Fact]
+        public void WhenSchemaIsValid_AddSchemaShouldNotThrowXmlValidationException_S3()
+        {
+            var validator = new Schematron.Validator(OutputFormatting.XML);
+
+            validator.AddSchema(TargetNamespace, XsdWithPartialSchemaLocation);
+
+            var ex = Xunit.Assert.Throws<Schematron.ValidationException>(() => validator.Validate(XmlContentLocation));
+
+            var result = ex.ToValidationResult();
+
+            Xunit.Assert.NotNull(result.xml);
+            Xunit.Assert.NotNull(result.schematron);
+        }
+
+        #endregion
+
+        [Fact]
+        public void WhileUsingDistinctAddSchemaMethods_ValidationResultShouldBeExactlyTheSame()
         {
             var validatorA = new Validator(OutputFormatting.XML);
             validatorA.AddSchema(XmlReader.Create(XsdLocation));
@@ -28,101 +102,45 @@ namespace Schematron.Tests
             var resultA = default(string);
             var resultB = default(string);
 
-            try
-            {
-                var result = validatorA.Validate(XmlReader.Create(XmlContentLocation));
-            }
-            catch (ValidationException ex)
-            {
-                resultA = ex.Message;
+            var exA = Xunit.Assert.Throws<Schematron.ValidationException>(() => validatorA.Validate(XmlReader.Create(XmlContentLocation)));
+            var exB = Xunit.Assert.Throws<Schematron.ValidationException>(() => validatorB.Validate(XmlReader.Create(XmlContentLocation)));
 
-                System.Diagnostics.Debug.WriteLine(ex.Message);                
-            }
+            Debug.WriteLine(exA.Message);
+            Debug.WriteLine(exB.Message);
 
-            try
-            {
-                var result = validatorB.Validate(XmlReader.Create(XmlContentLocation));
-            }
-            catch (ValidationException ex)
-            {
-                resultB = ex.Message;
-
-                Xunit.Assert.True(resultA == resultB);
-
-                System.Diagnostics.Debug.WriteLine(ex.Message);         
-            }
-
+            Xunit.Assert.True(resultA == resultB);
         }
+
+        #endregion
 
         [Fact]
-        public void ValidateShouldReturnSchematronValidationResultWhenSchematronConstraintsAreNotMet()
+        public void WhenIAddASchemaForSchematronValidationOnlyPurposes_AndASchemaWiseInvalidXmlDocument_ValidatorShouldThrow()
         {
-            //Arrange
-            var validator = new Validator(OutputFormatting.XML);
+            var validator = new Schematron.Validator(OutputFormatting.XML);
 
-            //Act
-            validator.AddSchema(TargetNamespace, XsdLocation);
+            var schematron = new Schema();
+            schematron.Load(XsdWithPartialSchemaLocation);
 
-            using (var doc = XmlReader.Create(XmlContentLocation))
+            validator.AddSchema(schematron);
+
+            var ex = Xunit.Assert.Throws<Schematron.ValidationException>(() => validator.Validate(XmlContentLocation));
+
+            Xunit.Assert.Null(ex.ToValidationResult().xml);
+            Xunit.Assert.NotNull(ex.ToValidationResult().schematron);
+        }
+    }
+
+    public static class SchematronExtensions
+    {
+        public static ValidationResult ToValidationResult(this Schematron.ValidationException ex)
+        {
+            var serializer = new XmlSerializer(typeof(Schematron.Serialization.SchematronValidationResultTempObjectModel.output));
+
+            using (var stream = new MemoryStream(System.Text.Encoding.Unicode.GetBytes(ex.Message)))
+            using (var reader = XmlReader.Create(stream))
             {
-                var result = default(IXPathNavigable);
-
-                try
-                {
-                    result = validator.Validate(doc);
-                }
-                catch (ValidationException ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-
-                    var serializer = new XmlSerializer(typeof(Schematron.Serialization.SchematronValidationResultTempObjectModel.output));
-
-                    using (var stream = new MemoryStream(System.Text.Encoding.Unicode.GetBytes(ex.Message)))
-                    using (var reader = XmlReader.Create(stream))
-                    {
-                        var obj = (Schematron.Serialization.SchematronValidationResultTempObjectModel.output)serializer.Deserialize(reader);
-
-                        // Assert
-
-
-                        Xunit.Assert.NotNull(obj);
-                        Xunit.Assert.NotNull(obj.xml);
-                        Xunit.Assert.NotNull(obj.schematron);
-                    }
-                }
+                return (Schematron.Serialization.SchematronValidationResultTempObjectModel.output)serializer.Deserialize(reader);
             }
-        }
-        
-        [Fact]
-        public void WhenUsingTheXmlReaderApproach_ToSupplyASchema_TypesFromImportsAreNotResolved()
-        {
-            // arrange
-            var validator = new Schematron.Validator();
-            
-            // act, (assert)
-            Xunit.Assert.Throws<XmlSchemaException>(() => validator.AddSchema(XmlReader.Create(XsdWithPartialSchemaLocation)));                      
-        }
-        
-        [Fact]
-        public void WhenUsingTheXmlSchemaSetBasedApproach_ToSupplyASchema_TypesFromImportsAreResolved()
-        {
-            // arrange
-            var validator = new Schematron.Validator();
-
-            var count = validator.XmlSchemas != null ? validator.XmlSchemas.Count : 0;
-
-            // act, (assert)
-            validator.AddSchema(TargetNamespace, XsdWithPartialSchemaLocation);
-
-            Xunit.Assert.True(validator.Schemas.Count == count + 1);
-
-            //var res = validator.Validate(XmlContentLocation);
-        }
-
-        //[Fact]
-        public void DoTheRawXmlValidation()
-        {
-            throw new NotImplementedException();
         }
     }
 }
